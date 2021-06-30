b254c2bb9b5af7d942a90767830dfd76487fa799
Amit-creator-coder <85750975+Amit-creator-coder@users.noreply.github.com>
2021-06-30 20:24:16 +0530

    Add files via upload

---
+++ spiralcircle.py
@@ -0,0 +1,6 @@
+import turtle
+cir=turtle.Turtle()
+turtle.bgcolor("red")
+for i in range(11):
+    cir.circle( i*10)
+turtle.done()
---
+++ triangle.py
@@ -0,0 +1,6 @@
+import turtle
+square=turtle.Turtle()
+for i in range(3):
+    square.forward(100)
+    square.left(120)
+turtle.done()
